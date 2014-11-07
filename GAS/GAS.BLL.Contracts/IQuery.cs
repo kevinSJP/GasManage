14ce87cd4555737a2ccdd5962f056ec0b7a8d926
kevinSJP <shaozhaosen@163.com>
2014-11-07 16:54:02 +0800

    GAS0.1
    Only Framework

---
+++ GAS/GAS.BLL.Contracts/IQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GAS.BLL.Contracts
+{
+    public interface IQuery
+    {
+    }
+}
---
+++ GAS/GAS.BLL.Host/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GAS.BLL.Host
+{
+    public class Class1
+    {
+    }
+}
